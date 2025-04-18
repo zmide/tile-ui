62b97e29b97b39ba474ace91220004c244aae024
shadcn <m@shadcn.com>
2025-04-18 21:20:03 +0400

    feat: add an example with css

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { OpenInV0Button } from "@/components/open-in-v0-button"
 import { HelloWorld } from "@/registry/new-york/blocks/hello-world/hello-world"
 import { ExampleForm } from "@/registry/new-york/blocks/example-form/example-form"
 import PokemonPage from "@/registry/new-york/blocks/complex-component/page"
-
+import { ExampleCard } from "@/registry/new-york/blocks/example-with-css/example-card"
 // This page displays items from the custom registry.
 // You are free to implement this with your own design as needed.
 
@@ -50,6 +50,18 @@ export default function Home() {
           </div>
           <div className="flex items-center justify-center min-h-[400px] relative">
             <PokemonPage />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
+          <div className="flex items-center justify-between">
+            <h2 className="text-sm text-muted-foreground sm:pl-3">
+              A login form with a CSS file.
+            </h2>
+            <OpenInV0Button name="example-with-css" className="w-fit" />
+          </div>
+          <div className="flex items-center justify-center min-h-[400px] relative">
+            <ExampleCard />
           </div>
         </div>
       </main>
